2eaa8e2a733551fa35cad843e1bbd8d92ed2760f
Aleksandra Wojcik <olawujcik@wp.pl>
2024-06-01 16:32:31 +0200

    zad3

--- main/src/main/java/pplab7/Main.java
+++ main/src/main/java/pplab7/Main.java
@@ -28,17 +28,18 @@ public class Main extends Application {
         searchField.setPromptText("Enter search phrase");
 
         resultArea = new TextArea();
-        resultArea.setPrefHeight(400); 
+        resultArea.setPrefHeight(400);
 
         Button browseButton = new Button("Browse");
         browseButton.setOnAction(e -> browseDirectory());
 
         Button searchButton = new Button("Search");
+        searchButton.setOnAction(e -> searchFiles());
 
         HBox hBox = new HBox(10, directoryPathField, browseButton);
-        VBox vBox = new VBox(10, hBox, searchField, searchButton, resultArea); 
+        VBox vBox = new VBox(10, hBox, searchField, searchButton, resultArea);
 
-        Scene scene = new Scene(vBox, 600, 400); 
+        Scene scene = new Scene(vBox, 600, 400);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
@@ -48,6 +49,37 @@ public class Main extends Application {
         File selectedDirectory = directoryChooser.showDialog(null);
         if (selectedDirectory != null) {
             directoryPathField.setText(selectedDirectory.getAbsolutePath());
+        }
+    }
+
+    private void searchFiles() {
+        String directoryPath = directoryPathField.getText();
+        if (directoryPath.isEmpty()) {
+            resultArea.setText("Please provide a directory path.");
+            return;
+        }
+
+        File directory = new File(directoryPath);
+        if (!directory.isDirectory()) {
+            resultArea.setText("The provided path is not a directory.");
+            return;
+        }
+
+        StringBuilder results = new StringBuilder();
+        listFilesInDirectory(directory, results);
+        resultArea.setText(results.toString());
+    }
+
+    private void listFilesInDirectory(File directory, StringBuilder results) {
+        File[] files = directory.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile()) {
+                    results.append(file.getAbsolutePath()).append("\n");
+                } else if (file.isDirectory()) {
+                    listFilesInDirectory(file, results);
+                }
+            }
         }
     }
 
